e4f7be9d8db245b5133c3deb3dd94f0faa052c45
Ben Davis <bendavis78@gmail.com>
2022-10-20 15:36:50 -0500

    Fixed config setting for `italic_comments`
    Config was pulling `italic_comments` setting from `g:italic_keywords` instead of `g:italic_comments`.

--- lua/darkplus/config.lua
+++ lua/darkplus/config.lua
@@ -14,7 +14,7 @@ end
 
 config = {
   transparent_background = opt('transparent_background', false),
-  italic_comments = opt('italic_keywords', true) and 'italic' or 'NONE',
+  italic_comments = opt('italic_comments', true) and 'italic' or 'NONE',
   italic_keywords = opt('italic_keywords', true) and 'italic' or 'NONE',
   italic_functions = opt('italic_function', false) and 'italic' or 'NONE',
   italic_variables = opt('italic_variables', true) and 'italic' or 'NONE',
